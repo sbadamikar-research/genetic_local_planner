b9dd950044cd0717e79e9318c2ecffab51eee19f
Sanchit Badamikar <researchbadamikar@gmail.com>
2025-11-26 19:29:23 -0700

    fix(Docker): improve docker dependencies

--- docker/ros1/build.sh
+++ docker/ros1/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")/../.."
-docker build -t plan_ga_ros1:latest -f docker/ros1/Dockerfile .
+docker build \
+    --network host \
+    -t plan_ga_ros1:latest \
+    -f docker/ros1/Dockerfile .
--- docker/ros2/build.sh
+++ docker/ros2/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")/../.."
-docker build -t plan_ga_ros2:latest -f docker/ros2/Dockerfile .
+docker build \
+    --network host \
+    -t plan_ga_ros2:latest \
+    -f docker/ros2/Dockerfile .
